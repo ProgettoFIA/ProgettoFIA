77df45df4ea8155a9c7000304327bae19af789f4
Salvatore-Mastellone <s.mastellone5@studenti.unisa.it>
2026-03-12 14:52:40 +0100

    Aggiunta del percorso Greedy alla mappa della classe visualization.py.

--- main.py
+++ main.py
@@ -33,20 +33,26 @@ if __name__ == "__main__":
         # Calcola percorsi
         risultati_finali = []
         for fam in popolazione:
-            #per l'algoritmo A*
+            # per l'algoritmo A* (lui valuta tutti i 'rifugi')
             rif_a, path_a, tempo_a, exec_a, nodi_a = scegli_rifugio_migliore(grafo, fam, rifugi, algoritmo="A*",tipo_euristica="euclidea")
-            #per l'algoritmo CU
-            rif_d,path_d,tempo_d,exec_d, nodo_d =scegli_rifugio_migliore(grafo,fam,rifugi,algoritmo="CU")
-            #per l'algortmo GREEDY
-            rif_g, path_g, tempo_g, exec_g, nodo_g = scegli_rifugio_migliore(grafo, fam, rifugi, algoritmo="GREEDY")
-            #facciamo un confronto tra i due
-            print(f"A* -> tempo dell'algoritmo: {exec_a:.4f}s")
-            print(f"GREEDY -> tempo dell'algoritmo: {exec_g:.4f}s")
-            print(f"CU -> tempo dell'algoritmo: {exec_d:.4f}s")
 
+            #Forzati i due algoritmi a trovare un percorso solo verso il rifugio scelto da A*
             if rif_a:
+                # per l'algoritmo CU
+                rif_d, path_d, tempo_d, exec_d, nodo_d = scegli_rifugio_migliore(grafo, fam, [rif_a], algoritmo="CU")
+
+                # per l'algoritmo GREEDY
+                rif_g, path_g, tempo_g, exec_g, nodo_g = scegli_rifugio_migliore(grafo, fam, [rif_a],  algoritmo="GREEDY")
+
+                #Confronto risultati
+                print(f"A* -> tempo dell'algoritmo: {exec_a:.4f}s")
+                print(f"GREEDY -> tempo dell'algoritmo: {exec_g:.4f}s")
+                print(f"CU -> tempo dell'algoritmo: {exec_d:.4f}s")
+
+
                 print(f"✅ ASSEGNATO: {rif_a.nome} in {tempo_a:.0f} minuti.")
-                risultati_finali.append((fam, rif_a, path_a, path_d, tempo_a))
+
+                risultati_finali.append((fam, rif_a, path_a, path_d, path_g, tempo_a))
 
         # Visualizza le mappe
         visualizza_simulazioni_personalizzate(grafo, risultati_finali)
--- visualization.py
+++ visualization.py
@@ -14,7 +14,7 @@ def visualizza_simulazioni_personalizzate(G, risultati):
     pos = nx.get_node_attributes(G, 'pos')
     colors = ['r', 'b', 'g', 'c', 'm']
 
-    for i, (famiglia, rifugio, percorso_astar, percorso_dijkstra, tempo) in enumerate(risultati):
+    for i, (famiglia, rifugio, percorso_astar, percorsoCu, percorsoGreedy, tempo) in enumerate(risultati):
         colore_famiglia = colors[i % len(colors)]
         fig, ax = plt.subplots(figsize=(10, 10))
         print(f"   -> Generazione mappa per: {famiglia.nome}...")
@@ -26,34 +26,47 @@ def visualizza_simulazioni_personalizzate(G, risultati):
             mx, my = converti_latlon_to_mercator(lat_n, lon_n)
             mercator_astar.append((mx, my))
 
-        #percorso dijkstra
-        mercator_dijkstra = []
-        for n in percorso_dijkstra:
+        #percorso CU
+        mercatorCu = []
+        for n in percorsoCu:
             lat_n, lon_n = pos[n]
             mx, my = converti_latlon_to_mercator(lat_n, lon_n)
-            mercator_dijkstra.append((mx, my))
+            mercatorCu.append((mx, my))
         px_a = [m[0] for m in mercator_astar]
         py_a = [m[1] for m in mercator_astar]
 
-        px_d = [m[0] for m in mercator_dijkstra]
-        py_d=  [m[1] for m in mercator_dijkstra]
+        px_d = [m[0] for m in mercatorCu]
+        py_d=  [m[1] for m in mercatorCu]
 
-        #percorso A*
-        ax.plot(px_a, py_a, color="blue", linewidth=3, alpha=0.9,
-                label=f"Percorso A*: {tempo:.0f} min", zorder=5)
+        #Percorso Greedy
+        mercatorGreedy = []
+        for n in percorsoGreedy:
+            lat_n, lon_n = pos[n]
+            mx, my = converti_latlon_to_mercator(lat_n, lon_n)
+            mercatorGreedy.append((mx, my))
+        px_g = [m[0] for m in mercatorGreedy]
+        py_g=  [m[1] for m in mercatorGreedy]
 
-        #percorso dijkstra
-        ax.plot(px_d, py_d, color="red", linewidth=6, alpha=0.8,
-                label=f"Percorso Dijkstra", zorder=3)
+        #Rappresentazione percorso A*
+        ax.plot(px_a, py_a, color="blue", linewidth=4, alpha=0.9,label=f"Percorso A*: {tempo:.0f} min", zorder=5)
+
+        #Rappresentazione percorso CU
+        ax.plot(px_d, py_d, color="red", linewidth=6, alpha=0.8, label=f"Percorso CU", zorder=3)
+
+        #Rappresentazione percorso Greedy
+        ax.plot(px_g, py_g, color="green", linewidth=4, alpha=0.8, label=f"Percorso Greedy", zorder=4)
+
         fam_x, fam_y = converti_latlon_to_mercator(famiglia.lat, famiglia.lon)
         rif_x, rif_y = converti_latlon_to_mercator(rifugio.lat, rifugio.lon)
 
         ax.scatter([fam_x], [fam_y], c=colore_famiglia, s=300, edgecolors='black', marker='o', zorder=6,
                    label="Posizione Iniziale")
-        ax.scatter([rif_x], [rif_y], c='green', s=400, marker='P', edgecolors='black', linewidth=2, zorder=6,
+        ax.scatter([rif_x], [rif_y], c='white', s=400, marker='P', edgecolors='black', linewidth=2, zorder=6,
                    label=f"DESTINAZIONE: {rifugio.nome}")
-        all_x=px_a+ px_d
-        all_y=py_a+ py_d
+
+        all_x = px_a + px_d + px_g
+        all_y = py_a + py_d + py_g
+
         min_x, max_x = min(all_x), max(all_x)
         min_y, max_y = min(all_y), max(all_y)
         margin_x = (max_x - min_x) * 0.2
@@ -70,12 +83,16 @@ def visualizza_simulazioni_personalizzate(G, risultati):
             print(f"Errore scaricamento sfondo: {e}")
 
         ax.set_axis_off()
-        plt.title(f"PIANO DI FUGA PERSONALIZZATO\nNucleo: {famiglia.nome} ({famiglia.descrizione})", fontsize=14,
-                  fontweight='bold')
-        #legenda per gli algoritmi
+        plt.title(f"PIANO DI FUGA PERSONALIZZATO\nNucleo: {famiglia.nome} ({famiglia.descrizione})", fontsize=14,fontweight='bold')
+
+        #Legenda per gli algoritmi
         astar_line=mlines.Line2D([],[], color='blue', linewidth=4, label= 'Algoritmo A*')
-        dijkstra_line=mlines.Line2D([],[], color='red', linewidth=4, linestyle='--', label='Algoritmo Dijkstra')
-        plt.legend(handles=[astar_line, dijkstra_line],
+
+        cuLine=mlines.Line2D([],[], color='red', linewidth=4, label='Algoritmo CU')
+
+        greedyLine = mlines.Line2D([], [], color='green', linewidth=4, label='Algoritmo Greedy')
+
+        plt.legend(handles=[astar_line, cuLine, greedyLine],
                    loc='upper left',
                    frameon=True,
                    shadow=True,
